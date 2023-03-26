e5138d0832baed081b4b454d2b3a64bee05f1af3
c-o-d-e <swapyuk@gmail.com>
2023-03-26 23:21:59 +0100

    fetch jobs data and test it

--- js/app.js
+++ js/app.js
@@ -2,7 +2,24 @@ const app = Vue.createApp({
     data() {
         return {
             imgHeight: 30,
+            jobsData: [],
+            categories: [],
         };
+    },
+    created() {
+        // get data from json file
+        fetch("/data/jobs.json")
+            .then(response => response.json())
+            .then(data => {
+                this.jobsData = data.jobs;
+
+                // create set to find unique values of categories
+                const categoriesSet = new Set();
+                this.jobsData.forEach(job => {
+                    categoriesSet.add(job.category);
+                });
+                this.categories = Array.from(categoriesSet);
+            });
     },
 
     computed: {
